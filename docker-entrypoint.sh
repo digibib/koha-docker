cbf7d9fecf9fb00cc2aff3b7c961e15a2d2727de
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-10-10 09:45:57 +0200

    Update docker-entrypoint (forgotten in last commit)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -43,9 +43,10 @@ envsubst < /templates/instance/apache.tmpl > /etc/apache2/sites-available/$KOHA_
 echo "Configuring SIPconfig.xml from templates and data from csv ..."
 door=`awk -f /templates/instance/SIPconfig.template_dooraccess.awk /templates/instance/SIPconfig.dooraccess.csv` ; \
 auto=`awk -f /templates/instance/SIPconfig.template_automats.awk /templates/instance/SIPconfig.automats.csv` ; \
+rfid=`awk -f /templates/instance/SIPconfig.template_rfid.awk /templates/instance/SIPconfig.rfid.csv` ; \
 inst=`awk -f /templates/instance/SIPconfig.template_institutions.awk /templates/instance/SIPconfig.automats.csv` ; \
 awk -v door="$door" -v auto="$auto" -v inst="$inst" \
-  '{gsub(/__TEMPLATE_DOOR_ACCOUNTS__/, door); gsub(/__TEMPLATE_AUTOMAT_ACCOUNTS__/, auto); gsub(/__TEMPLATE_INSTITUTIONS__/, inst) };1' \
+  '{gsub(/__TEMPLATE_DOOR_ACCOUNTS__/, door); gsub(/__TEMPLATE_AUTOMAT_ACCOUNTS__/, auto); gsub(/__TEMPLATE_RFID_ACCOUNTS__/, auto); gsub(/__TEMPLATE_INSTITUTIONS__/, inst) };1' \
   /templates/instance/SIPconfig.xml.tmpl | envsubst > /etc/koha/sites/$KOHA_INSTANCE/SIPconfig.xml
 
 echo "Configuring languages ..."
