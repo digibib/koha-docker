2d6d4730ddb16a631268fc69402adcdda47e4c51
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-10-27 09:49:36 +0200

    sync homebranch, not holdingbranch

--- files/cronjobs/holdingbranches.sh
+++ files/cronjobs/holdingbranches.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-koha-mysql $(koha-list --enabled) -N -B -e "SELECT biblioitemnumber,GROUP_CONCAT(DISTINCT holdingbranch) FROM items GROUP BY biblioitemnumber;"
+koha-mysql $(koha-list --enabled) -N -B -e "SELECT biblioitemnumber,GROUP_CONCAT(DISTINCT homebranch) FROM items GROUP BY biblioitemnumber;"
