0d08b42bb0df9e49c0fd5c5f5aba552c92aadfcc
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2016-11-28 09:33:06 +0100

    Don't forget to acutally debarr borrower

--- files/installer/triggers/autoKemnerSperring.sql
+++ files/installer/triggers/autoKemnerSperring.sql
@@ -4,11 +4,15 @@ CREATE TRIGGER autoKemnerSperring BEFORE UPDATE ON items
 FOR EACH ROW
 BEGIN
   IF NEW.itemlost = 12 AND (OLD.itemlost != 12 OR OLD.itemlost IS NULL) THEN
+    -- Add restriction comment: 'Sendt til kemner'
     INSERT INTO borrower_debarments (borrowernumber, type, comment, manager_id)
     (SELECT borrowernumber, 'MANUAL', 'Sendt til kemner',49393 FROM issues
      WHERE issues.itemnumber=NEW.itemnumber AND NOT EXISTS (
       SELECT * FROM borrower_debarments
       WHERE borrowernumber=borrowernumber AND comment='Sendt til kemner'));
+    -- Add the actuall debarrment on borrower
+    UPDATE borrowers JOIN issues USING (borrowernumber)
+    SET debarred='2999-01-01';
   END IF;
 END;//
 delimiter ;
