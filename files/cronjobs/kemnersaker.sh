0067421ea7d201999d94dd97be312a75d4ee0baf
Benjamin Rokseth <bensinober@gmail.com>
2016-11-28 16:12:06 +0100

    DEICH-488: crude rewrite of kemnersaker to separate job

---
+++ files/cronjobs/kemnersaker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# /root/kemnersaker.sh
+report="REPORT FROM TODAYS kemnersaker\n"
+
+add_new_items() {
+    # add new lines to kemnersaker where issue is more than 35 days overdue
+    RES=`cat <<-EOF | koha-mysql $(koha-list --enabled) --default-character-set=utf8
+        INSERT INTO kemnersaker
+          (issue_id,borrowernumber,itemnumber,status,timestamp)
+        SELECT i.issue_id,
+          i.borrowernumber,
+          i.itemnumber,
+          'new',
+          TIMESTAMP(NOW())
+        FROM issues i
+        JOIN items it ON (it.itemnumber=i.itemnumber)
+        JOIN borrowers b ON (b.borrowernumber=i.borrowernumber)
+        WHERE (TO_DAYS(now()) - TO_DAYS(i.date_due)) = '35'
+        AND it.itemlost = '12'
+        AND b.categorycode IN ('V','B','I')
+        AND NOT (b.categorycode = 'B' AND b.branchcode = 'fsme');
+
+EOF`
+    report+="new cases:\t$RES\n"
+}
+
+update_returned_items() {
+    # sets status to 'returned' for yesterdays returns
+    RES=`cat <<-EOF | koha-mysql $(koha-list --enabled) --default-character-set=utf8
+        UPDATE kemnersaker k
+        JOIN old_issues oi ON (oi.itemnumber=k.itemnumber)
+        SET k.status='returned'
+        WHERE DATE_SUB(DATE(NOW()), INTERVAL 1 DAY) = DATE(oi.returndate)
+        AND k.status IN ('new','sent');
+EOF`
+    report+="new returns:\t$RES\n"
+}
+
+block_patrons() {
+    RES=`cat <<-EOF | koha-mysql $(koha-list --enabled) --default-character-set=utf8
+        INSERT INTO borrower_debarments (borrowernumber, type, comment, manager_id)
+        (SELECT k.borrowernumber, 'MANUAL', 'Sendt til kemner',49393
+        FROM kemnersaker k
+        WHERE NOT EXISTS (
+            SELECT * FROM borrower_debarments
+            WHERE borrowernumber=borrowernumber AND comment='Sendt til kemner'
+        ) AND k.status IN ('new','sent'));
+        UPDATE borrowers b
+        JOIN kemnersaker k ON (k.borrowernumber=b.borrowernumber)
+        SET debarred='2999-01-01'
+        WHERE NOT EXISTS (
+            SELECT * FROM borrower_debarments
+            WHERE borrowernumber=borrowernumber AND comment='Sendt til kemner'
+        ) AND k.status IN ('new','sent');
+EOF`
+    report+="blocked patrons:\t$RES\n"
+}
+
+finish_jobs() {
+    echo "TODO: end cases handled manually until we get automated reports"
+}
+
+add_new_items
+update_returned_items
+block_patrons
+echo -e ${report}
