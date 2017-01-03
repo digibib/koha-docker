68574c9fcb80494f8d024fbdb7202099a437a4e0
Benjamin Rokseth <bensinober@gmail.com>
2017-01-03 20:43:19 +0100

    provision.sh revert

--- provision.sh
+++ provision.sh
@@ -48,19 +48,19 @@ else
   export MOUNTPATH=$KOHAPATH
 fi
 
-CMD="docker-compose -f common.yml KOHAPATH=$KOHAPATH GITREF=$GITREF "
+CMD="sudo KOHAPATH=${KOHAPATH} GITREF=${GITREF} docker-compose -f common.yml"
 
 case "$KOHAENV" in
   'dev')
-  ${CMD} -f dev.yml build koha
-  ${CMD} -f dev.yml up -d
+  $CMD -f dev.yml build koha
+  $CMD -f dev.yml up -d
   ;;
   'patched')
-  ${CMD} -f patched.yml build koha
-  ${CMD} -f patched.yml up -d
+  $CMD -f patched.yml build koha
+  $CMD -f patched.yml up -d
   ;;
   'build'|*)
-  ${CMD} build koha
-  ${CMD} up -d
+  $CMD build koha
+  $CMD up -d
   ;;
 esac
