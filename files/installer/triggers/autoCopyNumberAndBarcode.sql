b62a880cf4f71bbc29fdcb41159dca68aa0293c7
Benjamin Rokseth <bensinober@gmail.com>
2017-01-24 11:04:32 +0100

    Add automatic generation of barcode

---
+++ files/installer/triggers/autoCopyNumberAndBarcode.sql
@@ -0,0 +1,12 @@
+-- trigger for å øke eksemplarnummer på biblios automatisk
+-- bruker coalesce for å defaulte til 1 hvis ingen eksemplarer fra før
+-- legger også på strekkode inkrementert med 1
+DROP TRIGGER IF EXISTS autoCopyNumberAndBarcode;
+DELIMITER $$;
+CREATE TRIGGER autoCopyNumberAndBarcode BEFORE INSERT ON items
+FOR EACH ROW
+BEGIN
+  SET NEW.copynumber = (SELECT COALESCE((MAX(CAST(copynumber AS SIGNED)) + 1), 1) FROM items WHERE biblionumber = NEW.biblionumber);
+  SET NEW.barcode = (SELECT (MAX(barcode) + 1) FROM items);
+END $$;
+DELIMITER ;
