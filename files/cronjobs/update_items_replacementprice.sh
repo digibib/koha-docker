2b24d0e9c55eee6a5e33016e3b83c648e6d6e94d
Benjamin Rokseth <bensinober@gmail.com>
2017-06-14 14:23:56 +0200

    Update cronjob itemprice for koha 17.05 (moved marcxml)

--- files/cronjobs/update_items_replacementprice.sh
+++ files/cronjobs/update_items_replacementprice.sh
@@ -5,42 +5,42 @@
 report="REPORT FROM CRONJOB update_items_replacementprice.sh\n"
 
 RES="`cat <<-EOF | koha-mysql $(koha-list --enabled) --default-character-set=utf8 -N 2>&1
-  UPDATE items i
-  LEFT JOIN biblioitems b ON (i.biblionumber=b.biblionumber)
-  SET i.replacementprice =
-    (SELECT CASE EXTRACTVALUE(b.marcxml, '//record/datafield[@tag="337"]/subfield[@code="a"]/text()') /* mediaType */
-                WHEN 'Bok' THEN CASE
-                                    WHEN EXTRACTVALUE(b.marcxml, '//record/datafield[@tag="385"]/subfield[@code="a"]/text()') = 'Voksne' THEN '450.00' /* audience */
-                                    ELSE '300.00'
-                                END
-                WHEN 'Film' THEN '300.00'
-                WHEN 'Tegneserie' THEN '250.00'
-                WHEN 'Musikkopptak' THEN '300.00'
-                WHEN 'Lydbok' THEN '450.00'
-                WHEN 'Språkkurs' THEN '500.00'
-                WHEN 'Spill' THEN '500.00'
-                WHEN 'Dataspill' THEN '500.00'
-                WHEN 'Periodika' THEN '100.00'
-                WHEN 'Noter' THEN '250.00'
-                WHEN 'Brettspill' THEN '500.00'
-            END)
-  WHERE i.biblionumber=b.biblionumber /* EXCLUDED: 'REALIA' */
-    AND i.itype IN ('BOK',
-                    'DAGSLAAN',
-                    'EBOK',
-                    'FILM',
-                    'KART',
-                    'LYDBOK',
-                    'MUSIKK',
-                    'NOTER',
-                    'PERIODIKA',
-                    'SPILL',
-                    'SPRAAKKURS',
-                    'TOUKESLAAN',
-                    'UKESLAAN');
 
+    UPDATE items i
+    LEFT JOIN biblio_metadata bm ON (i.biblionumber=bm.biblionumber)
+    SET i.replacementprice =
+      (SELECT CASE EXTRACTVALUE(bm.metadata, '//record/datafield[@tag="337"]/subfield[@code="a"]/text()') /* mediaType */
+                  WHEN 'Bok' THEN CASE
+                                      WHEN EXTRACTVALUE(bm.metadata, '//record/datafield[@tag="385"]/subfield[@code="a"]/text()') = 'Voksne' THEN '450.00' /* audience */
+                                      ELSE '300.00'
+                                  END
+                  WHEN 'Film' THEN '300.00'
+                  WHEN 'Tegneserie' THEN '250.00'
+                  WHEN 'Musikkopptak' THEN '300.00'
+                  WHEN 'Lydbok' THEN '450.00'
+                  WHEN 'Språkkurs' THEN '500.00'
+                  WHEN 'Spill' THEN '500.00'
+                  WHEN 'Dataspill' THEN '500.00'
+                  WHEN 'Periodika' THEN '100.00'
+                  WHEN 'Noter' THEN '250.00'
+                  WHEN 'Brettspill' THEN '500.00'
+              END)
+    WHERE i.biblionumber=bm.biblionumber /* EXCLUDED: 'REALIA' */
+      AND i.itype IN ('BOK',
+                      'DAGSLAAN',
+                      'EBOK',
+                      'FILM',
+                      'KART',
+                      'LYDBOK',
+                      'MUSIKK',
+                      'NOTER',
+                      'PERIODIKA',
+                      'SPILL',
+                      'SPRAAKKURS',
+                      'TOUKESLAAN',
+                      'UKESLAAN');
 
-  SELECT ROW_COUNT();
+    SELECT ROW_COUNT();
 EOF`"
 
 report+="Updated items replacementprice:\t${RES}\n"
