0d5c227c3a1f25e4ea55f74f21b9ec7ce6b28a43
Benjamin Rokseth <bensinober@gmail.com>
2017-09-22 13:45:52 +0200

    Fix pull script for gitlab

--- koha-patched/pull.sh
+++ koha-patched/pull.sh
@@ -1,24 +1,18 @@
 #!/bin/bash -e
-# Pull Koha either from GITREF or from TAGGED version
-echo "GITREF: ${GITREF}"
 echo "KOHA_RELEASE: ${KOHA_RELEASE}"
 
-if [ -z "$GITREF" ] && [ -z "$KOHA_RELEASE" ]; then
-	echo "Need either GITREF or KOHA_RELEASE"
+if [ -z "$KOHA_RELEASE" ]; then
+	echo "Need KOHA_RELEASE"
 	exit 1
 fi
 
 mkdir -p /koha && cd /koha
-if [ "$GITREF" ]; then
-	curl -sSk -o koha.tar.gz https://codeload.github.com/digibib/Koha/legacy.tar.gz/${GITREF}
+RES=`curl -sSk -Iso /dev/null -w "%{http_code}" https://gitlab.deichman.no/digibib/Koha/repository/release/${KOHA_RELEASE}/archive.tar.gz`
+if [ $RES -eq 200 ]; then
+  curl -sSk -o koha.tar.gz https://gitlab.deichman.no/digibib/Koha/repository/release/${KOHA_RELEASE}/archive.tar.gz
 else
-  RES=`curl -sSk -Iso /dev/null -w "%{http_code}" https://codeload.github.com/digibib/Koha/tar.gz/release/${KOHA_RELEASE}`
-  if [ $RES -eq 200 ]; then
-    curl -sSk -o koha.tar.gz https://codeload.github.com/digibib/Koha/tar.gz/release/${KOHA_RELEASE}
-  else
-    echo "Trying old_releases archive..."
-    curl -sSk -o koha.tar.gz https://codeload.github.com/digibib/Koha/tar.gz/old_releases/${KOHA_RELEASE}
-  fi
+  echo "Failed getting tagged release ... giving up!"
+  exit 1
 fi
 
 tar -C /koha --strip-components=1 -xzf koha.tar.gz
