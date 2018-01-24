0857ea6478282e09c83f46b6511f389f09c6d14d
Benjamin Rokseth <bensinober@gmail.com>
2018-01-24 12:34:53 +0100

    Fix juvenile cronjob

--- files/cronjobs/unblock_juvenile_debarments.sh
+++ files/cronjobs/unblock_juvenile_debarments.sh
@@ -19,7 +19,7 @@ unblock_juveniles_with_kemnersak() {
         GROUP BY b.cardnumber HAVING issuecount = 0
         AND bd.comment IN ('Sendt til kemner',
                            'Regning') ) niceperson USING (borrowernumber)
-    SET b.debarred = NULL;
+    SET b.debarred = NULL, b.debarredcomment = NULL;
 
     DELETE bd
     FROM borrower_debarments bd
@@ -34,7 +34,7 @@ unblock_juveniles_with_kemnersak() {
           AND bd.type = 'MANUAL'
         GROUP BY b.cardnumber HAVING issuecount = 0
         AND bd.comment IN ('Sendt til kemner',
-                           'Regning') ) niceperson USING (borrowernumber);
+                           'Regning') ) niceperson USING (borrower_debarment_id);
 
     SELECT ROW_COUNT();
 EOF`"
@@ -42,34 +42,36 @@ EOF`"
 }
 
 unblock_juveniles_with_overdues() {
-  # Remove 'debarred' on patrons in category 'B'
+  # Remove borrower_debarments on patrons in category 'B'
   # with no more issues but which are blocked due to restriction set by overdue provess
+  # - then remove 'debarred' and 'debarredcomment' on 'B' patrons with no more debarments
   local RES="`cat <<-EOF | koha-mysql $(koha-list --enabled) --default-character-set=utf8 -N 2>&1
-    UPDATE borrowers b
-    JOIN
-      ( SELECT bd.*,
-               COUNT(iss.issue_id) AS issuecount
-        FROM borrower_debarments bd
-        JOIN borrowers b ON (b.borrowernumber=bd.borrowernumber)
-        JOIN kemnersaker k ON (k.borrowernumber=bd.borrowernumber)
-        JOIN issues iss USING (issue_id)
-        WHERE b.categorycode='B'
-          AND bd.type = 'OVERDUES'
-        GROUP BY b.cardnumber HAVING issuecount = 0 ) niceperson USING (borrowernumber)
-    SET b.debarred = NULL;
-
     DELETE bd
     FROM borrower_debarments bd
     JOIN
-      ( SELECT bd.*,
-               COUNT(iss.issue_id) AS issuecount
+      ( SELECT bd.*
         FROM borrower_debarments bd
-        JOIN borrowers b ON (b.borrowernumber=bd.borrowernumber)
-        JOIN kemnersaker k ON (k.borrowernumber=bd.borrowernumber)
-        JOIN issues iss USING (issue_id)
+        JOIN borrowers b USING (borrowernumber)
+        LEFT JOIN issues iss ON (iss.borrowernumber=b.borrowernumber)
+        LEFT JOIN kemnersaker k ON (k.borrowernumber=b.borrowernumber)
         WHERE b.categorycode='B'
-          AND bd.type = 'OVERDUES'
-        GROUP BY b.cardnumber HAVING issuecount = 0 ) niceperson USING (borrowernumber);
+          AND k.issue_id IS NULL
+          AND iss.issue_id IS NULL
+          AND bd.type = 'OVERDUES' ) niceperson USING (borrower_debarment_id);
+
+    UPDATE borrowers b
+    JOIN
+      ( SELECT b.borrowernumber
+        FROM borrowers b
+        LEFT JOIN borrower_debarments bd USING(borrowernumber)
+        LEFT JOIN kemnersaker k ON (k.borrowernumber=b.borrowernumber)
+        LEFT JOIN issues iss ON (iss.borrowernumber=b.borrowernumber)
+        WHERE b.categorycode='B'
+          AND bd.borrowernumber IS NULL
+          AND k.issue_id IS NULL
+          AND iss.issue_id IS NULL
+          AND (b.debarred IS NOT NULL OR b.debarred != "") ) niceperson USING (borrowernumber)
+    SET b.debarred = NULL, b.debarredcomment = NULL;
 
     SELECT ROW_COUNT();
 EOF`"
