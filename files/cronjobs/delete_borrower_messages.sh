60f3fb60388198b281e9fd3a2b1da9a98f555072
mattias.lundmark@deb.oslo.kommune.no <mattias.lundmark@deb.oslo.kommune.no>
2018-02-12 10:22:00 +0100

    DEICH-1097 added delete messages cron

---
+++ files/cronjobs/delete_borrower_messages.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# /cronjobs/delete_borrower_messages.sh --days
+#
+# This cronjob deletes messages sent to borrowers that are older than --days days
+
+usage() {
+  echo -e "\nUsage:\n$0 [-d|--days] \n"
+  exit 1
+}
+
+delete_messages() {
+    local DAYS=$1
+    echo "deleting messages sent to borrowers older than $DAYS days ..."
+    QUERY="DELETE from message_queue
+            WHERE (TO_DAYS(now()) - TO_DAYS(time_queued)) >= $DAYS AND status='sent'"
+    RES=`echo $QUERY | koha-mysql $(koha-list --enabled) -vv`
+    echo "$RES"  
+}
+
+case "$1" in
+    "")
+    usage
+    ;;
+  --days|-d)
+    shift
+    delete_messages $1
+    shift
+    ;;
+  --help|-h)
+    usage
+    ;;
+esac
